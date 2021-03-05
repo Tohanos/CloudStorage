3dbfdef9e01d51949ba967e488610114717d902f
polozov.artem <polozov.artem@otr.ru>
2021-03-05 22:34:20 +0300

    NIO + TelnetServer

--- src/main/java/server/Server.java
+++ src/main/java/server/Server.java
@@ -11,7 +11,7 @@ import java.util.concurrent.Executors;
 public class Server {
 	public Server() {
 		ExecutorService service = Executors.newFixedThreadPool(4);
-		try (ServerSocket server = new ServerSocket(1235)){
+		try (ServerSocket server = new ServerSocket(1236)){
 			System.out.println("Server started");
 			while (true) {
 				service.execute(new ClientHandler(server.accept()));
