af2d3089b3c5dc5cfdaa46f0b8b70d3c8580c58a
polozov.artem <polozov.artem@otr.ru>
2021-03-09 22:26:47 +0300

    netty

--- src/main/java/nio/NioTelnetServer.java
+++ src/main/java/nio/NioTelnetServer.java
@@ -3,18 +3,24 @@ package nio;
 import java.io.File;
 import java.io.IOException;
 import java.net.InetSocketAddress;
+import java.net.SocketAddress;
 import java.nio.ByteBuffer;
 import java.nio.channels.SelectionKey;
 import java.nio.channels.Selector;
 import java.nio.channels.ServerSocketChannel;
 import java.nio.channels.SocketChannel;
 import java.nio.charset.StandardCharsets;
+import java.util.HashMap;
+import java.util.Map;
 
 public class NioTelnetServer {
 	private final ByteBuffer buffer = ByteBuffer.allocate(512);
 
 	public static final String LS_COMMAND = "\tls          view all files from current directory\n";
 	public static final String MKDIR_COMMAND = "\tmkdir       view all files from current directory\n";
+	public static final String CHANGE_NICKNAME_COMMAND = "\tnick        change nickname\n";
+
+	private Map<String, SocketAddress> clients = new HashMap<>();
 
 	public NioTelnetServer() throws IOException {
 		ServerSocketChannel server = ServerSocketChannel.open(); // открыли
@@ -41,6 +47,8 @@ public class NioTelnetServer {
 
 	private void handleRead(SelectionKey key, Selector selector) throws IOException {
 		SocketChannel channel = (SocketChannel) key.channel();
+		SocketAddress client = channel.getRemoteAddress();
+		String nickname = "";
 		int readBytes = channel.read(buffer);
 		if (readBytes < 0) {
 			channel.close();
@@ -69,23 +77,36 @@ public class NioTelnetServer {
 					.replace("\n", "")
 					.replace("\r", "");
 			if ("--help".equals(command)) {
-				sendMessage(LS_COMMAND, selector);
-				sendMessage(MKDIR_COMMAND, selector);
+				sendMessage(LS_COMMAND, selector, client);
+				sendMessage(MKDIR_COMMAND, selector, client);
+				sendMessage(CHANGE_NICKNAME_COMMAND, selector, client);
+			} else if (command.startsWith("nick ")) {
+				nickname = command.split(" ")[1];
+				clients.put(nickname, client);
+				System.out.println("Client [" + client.toString() + "] changes nickname on [" + nickname + "]");
 			} else if ("ls".equals(command)) {
-				sendMessage(getFilesList().concat("\n"), selector);
+				sendMessage(getFilesList().concat("\n"), selector, client);
 			} else if ("exit".equals(command)) {
 				System.out.println("Client logged out. IP: " + channel.getRemoteAddress());
 				channel.close();
 				return;
 			}
 		}
-		sendName(channel);
+
+		for (Map.Entry<String, SocketAddress> clientInfo : clients.entrySet()) {
+			if (clientInfo.getValue().equals(client)) {
+				nickname = clientInfo.getKey();
+			}
+		}
+		sendName(channel, nickname);
 	}
 
-	private void sendName(SocketChannel channel) throws IOException {
+	private void sendName(SocketChannel channel, String nickname) throws IOException {
+		if (nickname.isEmpty()) {
+			nickname = channel.getRemoteAddress().toString();
+		}
 		channel.write(
-				ByteBuffer.wrap(channel
-						.getRemoteAddress().toString()
+				ByteBuffer.wrap(nickname
 						.concat(">: ")
 						.getBytes(StandardCharsets.UTF_8)
 				)
@@ -96,11 +117,13 @@ public class NioTelnetServer {
 		return String.join("\t", new File("server").list());
 	}
 
-	private void sendMessage(String message, Selector selector) throws IOException {
+	private void sendMessage(String message, Selector selector, SocketAddress client) throws IOException {
 		for (SelectionKey key : selector.keys()) {
 			if (key.isValid() && key.channel() instanceof SocketChannel) {
-				((SocketChannel) key.channel())
-						.write(ByteBuffer.wrap(message.getBytes(StandardCharsets.UTF_8)));
+				if (((SocketChannel) key.channel()).getRemoteAddress().equals(client)) {
+					((SocketChannel) key.channel())
+							.write(ByteBuffer.wrap(message.getBytes(StandardCharsets.UTF_8)));
+				}
 			}
 		}
 	}
@@ -112,7 +135,7 @@ public class NioTelnetServer {
 		channel.register(selector, SelectionKey.OP_READ, "some attach");
 		channel.write(ByteBuffer.wrap("Hello user!\n".getBytes(StandardCharsets.UTF_8)));
 		channel.write(ByteBuffer.wrap("Enter --help for support info\n".getBytes(StandardCharsets.UTF_8)));
-		sendName(channel);
+		sendName(channel, "");
 	}
 
 	public static void main(String[] args) throws IOException {
